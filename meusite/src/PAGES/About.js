6d64c51668429723eb5c2f141e9f80dd021c2ebc
Donizetti Oliveira <44945021+Donztt@users.noreply.github.com>
2021-08-28 21:41:14 -0300

    update dates

--- meusite/src/PAGES/About.js
+++ meusite/src/PAGES/About.js
@@ -13,26 +13,49 @@ function About() {
           <Fade top>
             <div id="Data">
               <h3>Donizetti José Fernando de Oliveira Junior</h3>
-              <p>Curso: Ciência da Computação (previsão de termino - Julho/2021)</p>
-              <p> Idade: 23 Anos</p>
+              <p>Curso: Ciência da Computação (previsão de termino - Dezembro/2021)</p>
+              <p> Idade: 24 Anos</p>
             </div>
             <div id="Experiences">
               <h3>Ideia Sistemas</h3>
               <div class="container">
                 <div class="row">
                   <div class="col align-self-start">
-                    <p>Linguagens de programação: VisualBasic 6/C#</p>
+                    <p>Linguagens de programação: VisualBasic 6 / C#</p>
                     <p>Banco de dados utilizados: SQL Server e MySQL</p>
-                    <p>Demais ferramentas utilizadas: XML e JSON</p>
+                    <p>Frameworks: Rest API</p>
                   </div>
                   <div class="col align-self-end">
-                    <p>Empresa focada em Desenvolvimento de sistemas para gestão empresarial (ERP),
-                    pude conhecer e aprender a linguagem Visual Basic 6 onde utilizei para começar minhas jornadas de trabalho.
-                    Nela adquiri experiências profissionais em programação e experiência em trabalho conjunto a uma equipe de profissionais da area.
+                    <p>
+                      Empresa focada em Desenvolvimento de sistemas para gestão empresarial (ERP),
+                      pude conhecer e aprender a linguagem Visual Basic 6 onde utilizei para começar minhas jornadas de trabalho.
+                      Nela adquiri experiências profissionais em programação e experiência em trabalho conjunto a uma equipe de profissionais da area.
                     </p>
                   </div>
                 </div>
               </div>
+            </div>
+            <div id="Experiences">
+              <h3>Trulogic</h3>
+              <div class="container">
+                <div class="row">
+                  <div class="col align-self-start">
+                    <p>Linguagens de programação: C# / Javascript</p>
+                    <p>Banco de dados utilizados: SQL Server e Postgres</p>
+                    <p>Frameworks: Rest API / ASP.NET / React-Native </p>
+                  </div>
+                  <div class="col align-self-end">
+                    <p>
+                      Um mar de conhecimento e companheirismo, a empresa Trulogic até então foi o lugar onde mais adquiri conhecimento.
+                      Trulogic é uma empresa focada em ouvir as necessidades ou idéias de seus clientes e transformar em realidade, com a diversidade de projetos,
+                      há também muita diversidade de conhecimento sendo adquirida sendo assim pude ter contato com várias ferramentas muito utilizadas atualmente e assim
+                      impulsionar minha jornada como desenvolvedor.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="About-Footer">
             </div>
           </Fade>
         </div>
--- meusite/src/PAGES/Home.js
+++ meusite/src/PAGES/Home.js
@@ -1,113 +1,255 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import NavBar from '../Components/NavBar';
-import Footer from '../Components/Footer';
-import '../CSS/Home.css';
-import Image from '../Photos/MyImages/eu.png';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
+import "../CSS/Home.css";
+import Image from "../Photos/MyImages/eu.png";
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
 
-import { FaJava } from 'react-icons/fa';
-import { FaUnity } from 'react-icons/fa';
-import { SiBlender } from 'react-icons/si';
-import { DiMysql } from 'react-icons/di';
-import { FaReact } from 'react-icons/fa';
-import { SiMicrosoftsqlserver } from 'react-icons/si';
-import { SiVisualstudiocode } from 'react-icons/si';
-import { ImGithub } from 'react-icons/im';
-import { SiHtml5 } from 'react-icons/si';
-import { SiCss3 } from 'react-icons/si';
-import { SiJavascript } from 'react-icons/si';
-import { SiOracle } from 'react-icons/si';
-import { SiCsharp } from 'react-icons/si';
+import { FaJava, FaReact, FaUnity } from "react-icons/fa";
+import { DiMysql } from "react-icons/di";
+import { ImGithub } from "react-icons/im";
+import { GiAutoRepair } from "react-icons/gi";
 
-//"@react-icons/all-files/fa/FaBeer";
+import {
+  SiAngular,
+  SiBootstrap,
+  SiDotNet,
+  SiSpring,
+  SiCsharp,
+  SiOracle,
+  SiJavascript,
+  SiCss3,
+  SiHtml5,
+  SiVisualstudiocode,
+  SiBlender,
+  SiMicrosoftsqlserver,
+  SiPostgresql,
+  SiBitbucket,
+  SiAdobephotoshop,
+  SiIntellijidea,
+  SiEclipseide,
+  SiTypescript,
+} from "react-icons/si";
+
 function Home() {
-    return (
-        <div>
-            <NavBar />
-                <div id="Home-Content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col align-self-start">
-                                <div className="container-md">
-                                    <Fade left cascade>
-                                        <div id="Welcome">
-                                            <h2><b>Bem vindo!</b></h2>
-                                            <h3><strong>Me chamo <mark id="Donizetti">Donizetti</mark>.</strong></h3>
-                                        </div>
-                                    </Fade>
-                                    <Fade left>
-                                        <div id="introduction">
-                                            <p>Desenvolvedor Full-Stack, interessado igualmente pelo Back-end e Front-end.</p>
-                                            <p>Trabalhei com desenvolvimento ERP utilizando Visual Basic 6, C# e SQL Server.</p>
-                                            <p>Tenho interesse em conhecer novas tecnologias, para ter uma base de conhecimento mais ampla e aprimorar minhas habilidades como programador</p>
-                                            <p>Este Portfólio também possui projetos pessoais que gostaria de apresentar a você!</p>
-                                            <Link to="/projetospessoais"><button type="button" class="btn btn-primary btn-lg btn-block">Conhecer Projetos</button></Link>
-                                        </div>
-                                    </Fade>
-                                </div>
-                            </div>
-                            <div class="col align-self-end">
-                                <Fade right>
-                                    <div id="Photo-Canvas">
-                                        <div id="Perfil-Photo">
-                                            <img src={Image} class="rounded mx-auto d-block" alt = ""></img>
-                                        </div>
-                                    </div>
-                                </Fade>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div id="general-content">
+      <NavBar />
+      <div id="Home-Content">
+        <div class="container">
+          <div class="row">
+            <div class="col align-self-start">
+              <div className="container-md">
+                <Fade left cascade>
+                  <div id="Welcome">
+                    <h2>
+                      <b>Bem vindo!</b>
+                    </h2>
+                    <h3>
+                      <strong>
+                        Me chamo <mark id="Donizetti">Donizetti</mark>.
+                      </strong>
+                    </h3>
+                  </div>
+                </Fade>
+                <Fade left>
+                  <div id="introduction">
+                    <p>
+                      Desenvolvedor Full-Stack, interessado igualmente pelo
+                      Back-end e Front-end.
+                    </p>
+                    <p>
+                      Tenho interesse em conhecer novas tecnologias, para ter
+                      uma base de conhecimento mais ampla e aprimorar minhas
+                      habilidades como programador
+                    </p>
+                    <p>
+                      Este Portfólio possui projetos pessoais que gostaria de
+                      apresentar a você!
+                    </p>
+                    <Link to="/projetospessoais">
+                      <button
+                        type="button"
+                        class="btn btn-primary btn-lg btn-block"
+                      >
+                        Conhecer Projetos
+                      </button>
+                    </Link>
+                  </div>
+                </Fade>
+              </div>
+            </div>
+            <div class="col align-self-end">
+              <Fade right>
+                <div id="Photo-Canvas">
+                  <div id="Perfil-Photo">
+                    <img
+                      src={Image}
+                      class="rounded mx-auto d-block"
+                      alt=""
+                    ></img>
+                  </div>
                 </div>
-                    <div id = "Skills">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col">
-                                    <h2><FaJava /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><SiCsharp /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><DiMysql /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><FaReact /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><SiMicrosoftsqlserver /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><SiVisualstudiocode /></h2>                           
-                                </div>
-                                <div class="col">
-                                    <h2><ImGithub /></h2>   
-                                </div>
-                                <div class="col">
-                                    <h2><SiHtml5 /></h2> 
-                                </div>
-                                <div class="col">
-                                    <h2><SiCss3 /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><SiJavascript /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><SiOracle /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><FaUnity /></h2>
-                                </div>
-                                <div class="col">
-                                    <h2><SiBlender /></h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            <Footer />
+              </Fade>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <Slide top>
+        <div id="Skills">
+          <div class="container">
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Java</p>
+                <FaJava id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Spring Boot</p>
+                <SiSpring id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">React</p>
+                <FaReact id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Angular</p>
+                <SiAngular id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">CSharp</p>
+                <SiCsharp id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">.NET</p>
+                <SiDotNet id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">MySQL</p>
+                <DiMysql id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">SQL Server</p>
+                <SiMicrosoftsqlserver id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">PostgresSQL</p>
+                <SiPostgresql id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Oracle SQL</p>
+                <SiOracle id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">VS Code</p>
+                <SiVisualstudiocode id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">IntelliJ</p>
+                <SiIntellijidea id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Eclipse</p>
+                <SiEclipseide id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Github</p>
+                <ImGithub id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Bitbucket</p>
+                <SiBitbucket id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">HTML 5</p>
+                <SiHtml5 id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">CSS 3</p>
+                <SiCss3 id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Javascript</p>
+                <SiJavascript id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Typescript</p>
+                <SiTypescript id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Bootstrap</p>
+                <SiBootstrap id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Unity</p>
+                <FaUnity id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Blender</p>
+                <SiBlender id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Photoshop</p>
+                <SiAdobephotoshop id="SkillIcon"/>
+              </h2>
+            </div>
+            <div class="col-2" id="tooltip">
+              <h2>
+                <p id="tooltiptext">Infraestrutura</p>
+                <GiAutoRepair id="SkillIcon"/>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </Slide>
+      <Footer />
+    </div>
+  );
 }
-
 
 export default Home;
